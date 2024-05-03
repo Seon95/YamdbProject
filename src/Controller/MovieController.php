f750404d39b13915e7d014aa456e252a7add7682
Marouan Ajtirah <marouan_39@hotmail.com>
2024-05-03 03:11:03 +0200

    fixed

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -8,31 +8,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class MovieController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    #[Route('/movies', name: 'movie_list')]
+    public function listMovies(EntityManagerInterface $entityManager): Response
     {
-        $this->entityManager = $entityManager;
+        $movies = $entityManager->getRepository(Movie::class)->findAll();
+
+        return $this->render('movies.html.twig', [
+            'movies' => $movies,
+        ]);
     }
 
-    #[Route('/movies', name: 'movie_list')]
-    public function listMovies(): Response
+    #[Route('/movies/{id}', name: 'movie_detail')]
+    public function detail(Movie $movie): Response
     {
-        // Get the movie repository using the injected entityManager property
-        $movieRepository = $this->entityManager->getRepository(Movie::class);
-
-        // Fetch all movies
-        $movies = $movieRepository->findAll();
-
-        // Output movie titles
-        $output = '';
-        foreach ($movies as $movie) {
-            $output .= sprintf("-%s\n", $movie->getTitle());
-        }
-
-        // Return a response with movie titles
-        return new Response($output);
+        return $this->render('movie_detail.html.twig', [
+            'movie' => $movie,
+        ]);
     }
 }
